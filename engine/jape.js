14eb39ab1534f5b349dbb5a3c35fcced0c4421f2
Mu57Di3 <mu57di3@gmail.com>
2014-08-05 19:59:20 +0400

    Доработал класс логер

--- engine/jape.js
+++ engine/jape.js
@@ -5,55 +5,85 @@
 (function (win,doc){
     var window = win || {};
     var document = doc || {};
-    var debug = true;
     var presentations = [];
 
-    function trace() {
-        if (!debug){ return false };
-        var now = new Date();
-        var timeStamp = '[' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + '.' + now.getMilliseconds() + '] ';
-        if (arguments.length > 1) {
-            console.log('benderlog - ' + timeStamp);
-            console.trace();
-            for (var i = 0, len = arguments.length; i < len; i++) {
-                if (typeof arguments[i] === 'object') {
-                    console.dir(arguments[i]);
-                } else {
-                    console.log(arguments[i]);
+    var $ = function (sel,el){
+        if (el!=undefined){
+            return el.querySelectorAll(sel);
+        }
+        return document.querySelectorAll(sel);
+    }
+
+    /**
+     * Класс для вывода логов
+     * @type {{_getTimeStamp: _getTimeStamp, info: info, log: log}}
+     */
+    trace = {
+        debug:true,
+
+        _getTimeStamp: function(){
+            var now = new Date();
+            return '[' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + '.' + now.getMilliseconds() + '] ';
+        },
+
+        info: function (){
+            if (!this.debug) { return false }
+            var timeStamp = this._getTimeStamp();
+            console.info('benderlog - ' + timeStamp + arguments[0]);
+        },
+
+        log:function (){
+            if (!this.debug) { return false }
+            var timeStamp = this._getTimeStamp();
+            if (arguments.length > 1) {
+                console.log('benderlog - ' + timeStamp);
+                console.trace();
+                for (var i = 0, len = arguments.length; i < len; i++) {
+                    if (typeof arguments[i] === 'object') {
+                        console.dir(arguments[i]);
+                    } else {
+                        console.log(arguments[i]);
+                    }
                 }
-            }
-        } else if (arguments.length = 1) {
-            if (typeof arguments[0] === 'object') {
-                console.log('benderlog - ' + timeStamp );
-                console.trace();
-                console.dir(arguments[0]);
-            } else {
-                console.log('benderlog - ' + timeStamp + arguments[0]);
-                console.trace();
-            }
+            } else if (arguments.length = 1) {
+                if (typeof arguments[0] === 'object') {
+                    console.log('benderlog - ' + timeStamp );
+                    console.trace();
+                    console.dir(arguments[0]);
+                } else {
+                    console.log('benderlog - ' + timeStamp + arguments[0]);
+                    console.trace();
+                }
 
 
+            }
         }
     }
 
 
-    window.jape = function (){
+    window.jape = function (el){
+        this.elem = el;
         this.slides = {}
     }
 
     jape.prototype = {
         init: function (){
-
-        },
-
-        run:function (){
-
+            trace.info('Инициализация презентации');
+            var list = $('section',this.elem);
+            trace.log(list);
         }
     }
 
     utils = {
         findPresentations: function (){
-            trace(document.querySelectorAll('presentation'));
+            var list  = document.querySelectorAll('presentation');
+            if (list.length > 0){
+                for (var i= 0,cnt = list.length;i<cnt;i++){
+                    presentations.push(new jape(list[i]))
+                    presentations[i].init();
+                }
+            }
+
         }
     }
 
